e92557b06b54a981ea49031cce14d472d759c29c
smtnd <smtnd@DESKTOP-NPUTHH2>
2022-02-01 17:37:26 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,66 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.DTos;
+using Core.Utilities.Results;
+using Business.Constants;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public IResult Add(Product product)
+        {
+            //business codes 
+            if (product.ProductName.Length < 2)
+            {
+                return new ErrorResult(Messages.NameInvalid);
+            }
+            _productDal.Add(product);
+            return new SuccessResult(Messages.Added);
+        }
+
+        public IDataResult<List<Product>> GetAll()
+        {
+            //İş kodları
+            //Yetkisi varmı ?
+            if (DateTime.Now.Hour == 23)
+            {
+                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            }
+
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.Listed);
+        }
+
+        public IDataResult<List<Product>> GetAllByCategoryId(int id)
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.CategoryId == id));
+        }
+
+        public IDataResult<Product> GetById(int productId)
+        {
+            return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
+        }
+
+        public IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max)
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.UnitPrice <= min && p.UnitPrice <= max));
+        }
+
+        public IDataResult<List<ProductDetailDto>> GetProductDetails()
+        {
+            return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
+        }
+
+    }
+}
---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string Added = "Eklendi !";
+        public static string Deleted = "Silindi !";
+        public static string Updated = "Güncellendi ! ";
+        public static string Listed = "Listelendi !";
+        public static string NameInvalid = "Karakter sayısı yetersiz !";
+        public static string Error = "Hata !";
+        public static string MaintenanceTime = "Sistem bakımda !";
+
+    }
+}
